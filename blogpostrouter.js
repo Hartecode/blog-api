e0338c2d40235c5bdd32e1918a6a796d3eaf4e2f
Sean Harte <scsharte@gmail.com>
2018-03-01 22:47:05 -0500

    set up get

--- blogpostrouter.js
+++ blogpostrouter.js
@@ -1,75 +1,93 @@
 const express = require('express');
+const mongoose = require('mongoose');
 const router = express.Router();
 
 const bodyParser = require('body-parser');
-const jsonParser = bodyParser.json();
 
 
 const {BlogPosts} = require('./models');
 
-//blog post samples//
-BlogPosts.create('start','This is the start of the blog','Sean Harte');
-BlogPosts.create('go','This is the second test','Sean Harte');
-BlogPosts.create('keeo going','This is the third test','Sean Harte');
+router.use(bodyParser.json());
 
-
+//get posts
 router.get('/',(req, res) =>{
-	res.json(BlogPosts.get());
+	BlogPosts
+		.find()
+		.then(posts => {
+			res.json(posts.map(post => post.serialize()));
+		})
+		.catch( err => {
+			console.error(err);
+			res.status(500).json({ message: 'Internal server error'});
+		});
 });
 
-router.post('/', jsonParser, (req, res) => {
-	const requiredfeild = ['title','content', 'author'];
-	//check if the right keys are in the request
-	console.log(res.body);
-	for(let i = 0; i < requiredfeild.lenght; i++){
-		const feild = requiredfeild[i];
-		if(!(feild in req.body)){
-			const message = `Missing \`${feild}\` in request body`;
-			console.error(message);
-			return res.status(400).send(message);
-		}
-	}
-
-	const item = BlogPosts.create(req.body.title, req.body.content, req.body.author);
-	res.status(201).json(item);
+// can also request by ID
+router.get('/:id', (req, res) => {
+  BlogPosts
+    // this is a convenience method Mongoose provides for searching
+    // by the object _id property
+    .findById(req.params.id)
+    .then(post => res.json(post.serialize()))
+    .catch(err => {
+      console.error(err);
+      res.status(500).json({ message: 'Internal server error' });
+    });
 });
 
+// router.post('/', jsonParser, (req, res) => {
+// 	const requiredfeild = ['title','content', 'author'];
+// 	//check if the right keys are in the request
+// 	console.log(res.body);
+// 	for(let i = 0; i < requiredfeild.lenght; i++){
+// 		const feild = requiredfeild[i];
+// 		if(!(feild in req.body)){
+// 			const message = `Missing \`${feild}\` in request body`;
+// 			console.error(message);
+// 			return res.status(400).send(message);
+// 		}
+// 	}
 
-router.put('/:id', jsonParser, (req, res) => {
-	const requiredfeild = ['title','content', 'author', 'id'];
-	for(let i = 0; i < requiredfeild.lenght; i++){
-		const feild = requiredfeild[i];
-		if(!(feild in req.body)){
-			const message = `Missing \`${feild}\` in request body`;
-			console.error(message);
-			return res.status(400).send(message);
-		}
-	}
+// 	const item = BlogPosts.create(req.body.title, req.body.content, req.body.author);
+// 	res.status(201).json(item);
+// });
 
-	if (req.params.id !== req.body.id) {
-	    const message = `Request path id (${req.params.id}) and request body id (${req.body.id}) must match`;
-	    console.error(message);
-	    return res.status(400).send(message);
-	}
-	console.log(`Updating shopping list item \`${req.params.id}\``);
-	console.log(res.body);
 
-	BlogPosts.update({
-		id: req.params.id,
-		title: req.body.title,
-		content: req.body.content,
-		author: req.body.author
-	});
+// router.put('/:id', jsonParser, (req, res) => {
+// 	const requiredfeild = ['title','content', 'author', 'id'];
+// 	for(let i = 0; i < requiredfeild.lenght; i++){
+// 		const feild = requiredfeild[i];
+// 		if(!(feild in req.body)){
+// 			const message = `Missing \`${feild}\` in request body`;
+// 			console.error(message);
+// 			return res.status(400).send(message);
+// 		}
+// 	}
 
-	res.status(204).end();
-});
+// 	if (req.params.id !== req.body.id) {
+// 	    const message = `Request path id (${req.params.id}) and request body id (${req.body.id}) must match`;
+// 	    console.error(message);
+// 	    return res.status(400).send(message);
+// 	}
+// 	console.log(`Updating shopping list item \`${req.params.id}\``);
+// 	console.log(res.body);
 
-router.delete('/:id', (req, res) => {
-	console.log(req.params.id);
-	BlogPosts.delete(req.params.id);
-	console.log(`You have deleted post id:${req.params.id}`);
-	res.status(204).end();
-});
+// 	BlogPosts.update({
+// 		id: req.params.id,
+// 		title: req.body.title,
+// 		content: req.body.content,
+// 		author: req.body.author
+// 	});
+
+// 	res.status(204).end();
+// });
+
+// router.delete('/:id', (req, res) => {
+// 	console.log(req.params.id);
+// 	BlogPosts.delete(req.params.id);
+// 	console.log(`You have deleted post id:${req.params.id}`);
+// 	res.status(204).end();
+// });
 
 
 module.exports = router;
