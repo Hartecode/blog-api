89741f2bcdf379629bb21dd66df07f1e8533bbc5
Sean Harte <scsharte@gmail.com>
2018-03-01 23:30:47 -0500

    add put to api

--- blogpostrouter.js
+++ blogpostrouter.js
@@ -61,36 +61,40 @@ router.post('/', (req, res) => {
     });
 });
 
+//this updates a blog item
+router.put('/:id', (req, res) => {
+  // ensure that the id in the request path and the one in request body match
+  if (!(req.params.id && req.body.id && req.params.id === req.body.id)) {
+    const message = (
+      `Request path id (${req.params.id}) and request body id ` +
+      `(${req.body.id}) must match`);
+    console.error(message);
+    return res.status(400).json({ message: message });
+  }
 
-// router.put('/:id', jsonParser, (req, res) => {
-// 	const requiredfeild = ['title','content', 'author', 'id'];
-// 	for(let i = 0; i < requiredfeild.lenght; i++){
-// 		const feild = requiredfeild[i];
-// 		if(!(feild in req.body)){
-// 			const message = `Missing \`${feild}\` in request body`;
-// 			console.error(message);
-// 			return res.status(400).send(message);
-// 		}
-// 	}
+  // we only support a subset of fields being updateable.
+  // if the user sent over any of the updatableFields, we udpate those values
+  // in document
+  const toUpdate = {};
+  const requiredfeild = ['title','content', 'author', 'id'];
 
-// 	if (req.params.id !== req.body.id) {
-// 	    const message = `Request path id (${req.params.id}) and request body id (${req.body.id}) must match`;
-// 	    console.error(message);
-// 	    return res.status(400).send(message);
-// 	}
-// 	console.log(`Updating shopping list item \`${req.params.id}\``);
-// 	console.log(res.body);
+  requiredfeild.forEach(field => {
+    if (field in req.body) {
+      toUpdate[field] = req.body[field];
+    }
+  });
 
-// 	BlogPosts.update({
-// 		id: req.params.id,
-// 		title: req.body.title,
-// 		content: req.body.content,
-// 		author: req.body.author
-// 	});
+  BlogPosts
+    // all key/value pairs in toUpdate will be updated -- that's what `$set` does
+    .findByIdAndUpdate(req.params.id, { $set: toUpdate })
+    .then(() => {
+    	console.log(`Updating blog item \`${req.params.id}\``);
+    	res.status(204).end();
+    })
+    .catch(err => res.status(500).json({ message: 'Internal server error' }));
+});
 
-// 	res.status(204).end();
-// });
-
+//this will delete a selcted item
 router.delete('/:id', (req, res) => {
 	console.log(req.params.id);
 	BlogPosts
