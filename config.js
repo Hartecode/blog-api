725d191975cad9ddcc61f4d01e2a20d70e9f5475
Sean Harte <scsharte@gmail.com>
2018-03-03 09:04:34 -0500

    added test-url to config, add seed-data, add server test folder/file

--- config.js
+++ config.js
@@ -1,5 +1,7 @@
 'use strict';
 //connects to the database blogPosts
-exports.DATABASE_URL = process.env.DATABASE_URL || 'mongodb://localhost/blogPosts';
+exports.DATABASE_URL = process.env.DATABASE_URL || 'mongodb://localhost/blog-posts';
 
+//connects to the testing database
+exports.TEST_DATABASE_URL = process.env.TEST_DATABASE_URL || 'mongodb://localhost/test-blog-posts';
 exports.PORT = process.env.PORT || 8080;
